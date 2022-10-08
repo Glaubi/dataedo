acbc667f25683b94ff74d0f0ec618fb960561b29
Glaubi <michal_glaubert6@o2.pl>
2022-10-08 13:53:20 +0200

    Update users.php

--- users.php
+++ users.php
@@ -1,54 +1,61 @@
 <?php
 
-public function updateUsers($users)
-{
-	foreach ($users as $user) {
-		try {
-			if ($user['name'] && $user['login'] && $user['email'] && $user['password'] && strlen($user['name']) >= 10)
-				DB::table('users')->where('id', $user['id'])->update([
-					'name' => $user['name'],
-					'login' => $user['login'],
-					'email' => $user['email'],
-					'password' => md5($user['password'])
-				]);
-		} catch (Throwable $e) {
-			return Redirect::back()->withErrors(['error', ["We couldn't update user: " . $e->getMessage()]]);
-		}
-	}
-	return Redirect::back()->with(['success', 'All users updated.']);
-}
+class Users{
 
-public function storeUsers($users)
-{
-    foreach ($users as $user) {
-        try {
-			if ($user['name'] && $user['login'] && $user['email'] && $user['password'] && strlen($user['name']) >= 10)
-				DB::table('users')->insert([
-					'name' => $user['name'],
-					'login' => $user['login'],
-					'email' => $user['email'],
-					'password' => md5($user['password'])
-            ]);
-        } catch (Throwable $e) {
-            return Redirect::back()->withErrors(['error', ["We couldn't store user: " . $e->getMessage()]]);
+    public function updateUsers($users)
+    {
+        foreach ($users as $user) {
+            try {
+                if ($user['name'] && $user['login'] && $user['email'] && $user['password'] && strlen($user['name']) >= 10)
+                    DB::table('users')->where('id', $user['id'])->update([
+                        'name' => $user['name'],
+                        'login' => $user['login'],
+                        'email' => $user['email'],
+                        'password' => md5($user['password'])
+                    ]);
+            } catch (Throwable $e) {
+                return Redirect::back()->withErrors(['error', ["We couldn't update user: " . $e->getMessage()]]);
+            }
         }
+        return Redirect::back()->with(['success', 'All users updated.']);
     }
-    $this->sendEmail($users);
-    return Redirect::back()->with(['success', 'All users created.']);
-}
 
-private function sendEmail($users)
-{
-    foreach ($users as $user) {
-        $message = 'Account has beed created. You can log in as <b>' . $user['login'] . '</b>';
-        if ($user['email']) {
-            Mail::to($user['email'])
-                ->cc('support@company.com')
-                ->subject('New account created')
-                ->queue($message);
+    public function storeUsers($users)
+    {
+        foreach ($users as $user) {
+            try {
+                if ($user['name'] && $user['login'] && $user['email'] && $user['password'] && strlen($user['name']) >= 10)
+                    DB::table('users')->insert([
+                        'name' => $user['name'],
+                        'login' => $user['login'],
+                        'email' => $user['email'],
+                        'password' => md5($user['password'])
+                ]);
+            } catch (Throwable $e) {
+                return Redirect::back()->withErrors(['error', ["We couldn't store user: " . $e->getMessage()]]);
+            }
         }
+        $this->sendEmail($users);
+        return Redirect::back()->with(['success', 'All users created.']);
     }
-    return true;
+
+    private function sendEmail($users)
+    {
+        foreach ($users as $user) {
+            if (isset($user['login']) && isset($user['email'])) {
+                try {
+                    $message = 'Account has beed created. You can log in as <b>' . $user['login'] . '</b>';
+                    Mail::to($user['email'])
+                        ->cc('support@company.com')
+                        ->subject('New account created')
+                        ->html($message);
+                }catch (Throwable $e) {
+                    echo $e->getMessage(), "\n";
+                }
+            }
+        }
+        return true;
+    }
 }
 
 ?>
